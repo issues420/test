16381083ef51fee6b6dafc4abae73504911169e4
issues420 <sales@glhydroco.com>
2018-01-31 15:13:22 -0500

    Update arconai.py
    rework of kodi plugin for streamlink

--- arconai.py
+++ arconai.py
@@ -20,35 +20,35 @@ class ArconaiTv(Plugin):
        def can_handle_url(cls, url):
         	return cls._url_re.match(url)
 
-def _get_streams(self):
-       headers = {
-              'User-Agent': useragents.CHROME,
-              'Referer': self.url
-       }
+       def _get_streams(self):
+              headers = {
+                     'User-Agent': useragents.CHROME,
+                     'Referer': self.url
+              }
 
-       r = requests.get(url)
-       html_text = r.text
-       soup = BeautifulSoup(html_text, 'html.parser')
-       scripts = soup.find_all('script')
-       for script in scripts:
-        	if script.string is not None:
-			if "document.getElementsByTagName('video')[0].volume = 1.0;" in script.string: 
-				code = script.string
+              res = http.get(self.url)
+              html_text = res.text
+              soup = BeautifulSoup(html_text, 'html.parser')
+              scripts = soup.find_all('script')
+              for script in scripts:
+                     if script.string is not None:
+                            if "document.getElementsByTagName('video')[0].volume = 1.0;" in script.string: 
+                                   code = script.string
 			# Here is the call to the first part of the deobfuscation i.e. getting packed code
-			code = aadecode(code)
-			code = code.aadecode()
-			if not code.replace(' ', '').startswith('eval(function(p,a,c,k,e,'):
-				code = 'fail'
-				break
-			else:
-				code = 'fail'
-		else:
-			code = 'fail'
+			           code = aadecode(code)
+			           code = code.aadecode()
+			           if not code.replace(' ', '').startswith('eval(function(p,a,c,k,e,'):
+				          code = 'fail'
+                                   break
+			    else:
+				   code = 'fail'
+                     else:
+			    code = 'fail'
 	#The second part of deobfuscation occurs here. Using module jsbeautifier.
-       if code != 'fail':
-              unpacked = packer.unpack(code)
-              video_location = unpacked[unpacked.rfind('http'):unpacked.rfind('m3u8')+4]
-              url = video_location
+              if code != 'fail':
+                     unpacked = packer.unpack(code)
+                     video_location = unpacked[unpacked.rfind('http'):unpacked.rfind('m3u8')+4]
+                     url = video_location
               if url:
                      self.logger.debug('HLS URL: {0}'.format(url))
                      yield 'live', HLSStream(self.session, url, headers=headers)
